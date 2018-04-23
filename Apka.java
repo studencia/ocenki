7652c06cfba844e52a28dcb45a65f3cd2114b22a
Deemorgan <deemorgan@wp.pl>
2018-04-23 23:36:14 +0200

    Update Apka.java

--- Apka.java
+++ Apka.java
@@ -11,10 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author BlackGreen
- */
+
 public class Apka extends Application {
     
     @Override
