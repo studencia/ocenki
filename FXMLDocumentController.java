ac00a6096d5593baa515916caa8382d04a2faa88
darbro96 <dbronikowsk@gmail.com>
2018-04-24 18:05:19 +0200

    Update FXMLDocumentController.java

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -45,6 +45,42 @@ public class FXMLDocumentController implements Initializable {
         }
     }
     
+    public void aktualizuj() {
+        Connection polaczenie = polacz();
+        Statement st;
+        try {
+            st = polaczenie.createStatement();
+            String SQL = "SELECT COUNT(nrIndeksu) as Ile FROM dane;";
+            ResultSet rs = st.executeQuery(SQL);
+            rs.next();
+            String ilosc = rs.getString("Ile");
+            int wszyscy = rs.getInt("Ile");
+            pasekWszyscy.setText(ilosc);
+            SQL = "SELECT COUNT(nrIndeksu) as ILE FROM dane WHERE ocenaKoncowa>2;";
+            rs = st.executeQuery(SQL);
+            rs.next();
+            ilosc = rs.getString("Ile");
+            int zdane = rs.getInt("Ile");
+            pasekZdane.setText(ilosc);
+            SQL = "SELECT COUNT(nrIndeksu) as ILE FROM dane WHERE ocenaKoncowa=2;";
+            rs = st.executeQuery(SQL);
+            rs.next();
+            ilosc = rs.getString("Ile");
+            pasekNiezdane.setText(ilosc);
+            if (wszyscy != 0) {
+                double stosunek = ((double) zdane / (double) wszyscy) * 100;
+                DecimalFormat dec = new DecimalFormat("#0");
+                pasekProcenty.setText(dec.format(stosunek) + "%");
+            } else {
+                pasekProcenty.setText("0%");
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        rozlacz(polaczenie);
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
